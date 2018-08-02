66c38b63d21212c562aef847a7bf48667aed7f7a
kasia brzozowicz <k.m.brzozowicz@gmail.com>
2018-08-02 16:06:01 +0200

    initiel commit

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Course Administration</h1>
+        <p> React, Redux, and react Router in ES6 for ultra-responsive webapps.</p>
+      </div>
+    )
+  }
+}
+
+export default HomePage;
