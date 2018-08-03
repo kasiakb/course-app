5e2b1a50c0b49ce9bc6a8e519eff490ecf42e621
kasia brzozowicz <k.m.brzozowicz@gmail.com>
2018-08-03 16:21:29 +0200

    Add course title to state

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
 
 class CoursesPage extends Component {
+  constructor (props,context) {
+    super(props, context);
+
+    this.state = {
+      course: {title: ''}
+    };
+  }
+
+  onTitleChange(e) {
+    e.preventDefault();
+    this.setState({
+      course: {title: e.target.value}
+    })
+  }
+
+  onClickSave(e) {
+    // e.preventDefault();
+    alert(`Saving ${this.state.course.title}`)
+  }
+
   render () {
+    console.log('title', this.state.course.title)
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input
+        type="text"
+        onChange={(e) => this.onTitleChange(e)}
+        />
+
+        <button onClick={(e) => this.onClickSave(e)}>
+          Save
+        </button>
+
       </div>
     )
   }
