5e6eadf38de184d64aa435ed9bc17544d8b4a18f
kasia brzozowicz <k.m.brzozowicz@gmail.com>
2018-08-30 14:12:16 +0200

    Display correctly course category

--- src/components/main/CourseListRow.js
+++ src/components/main/CourseListRow.js
@@ -8,7 +8,7 @@ const CourseListRow = ({course, deleteCourse, completedCourse}) => {
       <td><a href={course.watchHref} target="_blank">Watch</a></td>
       <td><Link to={'/course/' + course.id}>{course.title}</Link></td>
       <td>{course.authorId}</td>
-      <td>{course.caregory}</td>
+      <td>{course.category}</td>
       <td>{course.length}</td>
       <td>
         <input
